39c68efb1797f2b83884487b511bec15bd470405
treezyxd <thalesmaltaxd@gmail.com>
2023-05-12 00:58:49 -0300

    api done

--- bd.py
+++ bd.py
@@ -1,4 +1,4 @@
-cars = [
+Cars = [
     {
         'id': 1,
         'marca': 'Fiat',
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from bd import Cars
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/carros', methods=['GET'])
+def get_cars():
+    return jsonify(
+            message='Lista de carros',
+            data=Cars
+        )
+
+@app.route('/carros/<int:id>', methods=['GET'])
+def get_cars_by_id(id):
+    for car in Cars:
+        if(car.get('id') == id):
+            return jsonify(car)
+
+@app.route('/carros', methods=['POST'])
+def create_car():
+    car = request.json
+    Cars.append(car)
+    return jsonify(
+            message='Carro cadastrado com sucesso',
+            carro=car
+        )
 
 app.run()
